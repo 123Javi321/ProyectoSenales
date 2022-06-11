9cf8f3e99d39fee7b84f8210f514100370b2eb24
Javi <javi.castaneda11@gmail.com>
2022-06-11 17:21:35 -0600

    Ventana de inicio

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 #Grupo 1
-print('hola prueba')
-print('prueba 2')
+#Librerias a utilizar
+#import numpy as np
+from tkinter import Tk, Label, Button
+
+#clase de ventana principal
+class VentanaInicio:
+    def __init__(self, master):
+        self.master = master
+        master.title("Inicio - Proyecto ASS - G1")
+        master.geometry("500x250")
+
+        self.etiqueta = Label(master, text="Proyecto Analisis de Senales y Sistemas", font=('Arial', 16))
+        self.etiqueta.pack(pady=20)
+
+        self.botonInicio = Button(master, text="Inicio", command=self.iniciar, width=20)
+        self.botonInicio.pack(pady=10)
+
+        self.botonCreditos = Button(master, text="Creditos", command=self.creditos, width=20)
+        self.botonCreditos.pack(pady=10)
+
+        self.botonSalir = Button(master, text="Salir", command=master.quit, width=20)
+        self.botonSalir.pack(pady=10)
+
+    def iniciar(self):
+        print('Importar audios')
+
+    def creditos(self):
+        print('Grupo 1')
+
+root = Tk()
+miVentana = VentanaInicio(root)
+root.mainloop()
