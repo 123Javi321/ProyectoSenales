a9134735bc95eded10bee8e25c1386e4d1a119fa
Javi <javi.castaneda11@gmail.com>
2022-06-12 00:35:55 -0600

    Ventana de inicio y audios

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 #Grupo 1
 #Librerias a utilizar
 #import numpy as np
-from tkinter import Tk, Label, Button
+from tkinter import *
+
+class ventanaImportar:
+    def __init__(self):
+        #Creacion de nueva ventana para importar audios
+        ventanaImportar = Toplevel()
+        ventanaImportar.geometry("500x250")
+        ventanaImportar.title("Importar audios")
+
+        ventanaImportar.etiqueta = Label(ventanaImportar, text="Importación de audios", font=('Arial', 16))
+        ventanaImportar.etiqueta.pack(pady=20)
+
+        ventanaImportar.botonImportar = Button(ventanaImportar, text="Importar", command=self.importar, width=20)
+        ventanaImportar.botonImportar.pack(pady=30)
+
+    def importar(self):
+            print('importar')
 
 #clase de ventana principal
 class VentanaInicio:
@@ -21,13 +37,26 @@ class VentanaInicio:
 
         self.botonSalir = Button(master, text="Salir", command=master.quit, width=20)
         self.botonSalir.pack(pady=10)
-
+    #Metodo para el boton de inicio
     def iniciar(self):
         print('Importar audios')
+        #este comando cierra VentanaInicio
+        importarAudio = ventanaImportar()
+        root.withdraw()
 
+
+    #Metodo para el boton de creditos
     def creditos(self):
         print('Grupo 1')
+        #Creacion de nueva ventana para los creditos
+        ventanaCreditos = Toplevel()
+        ventanaCreditos.geometry("500x250")
+        ventanaCreditos.title("Integrantes")
+        #este comando cierra VentanaInicio
+        root.withdraw()
 
 root = Tk()
 miVentana = VentanaInicio(root)
 root.mainloop()
+
+
